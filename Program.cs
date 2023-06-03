77870731124816eba42b89fdd270a378a98a66d3
abhinav9691 <81373991+abhinav9691@users.noreply.github.com>
2023-06-03 09:28:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using QueueTest;
+
+QueueEx ex = new QueueEx();
+ex.Enqueue(20);
+ex.Enqueue(40);
+ex.Enqueue(50);
+ex.Enqueue(60);
+ex.Display();
---
+++ QueueEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTest
+{
+    internal class QueueEx
+    {
+        Node front;
+        Node rear;
+
+        public void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if(front == null)
+            {
+                rear = newnode;
+                front = rear;
+            }
+            else
+            {
+                rear.Next = newnode;
+                rear = rear.Next;
+            }
+        }
+
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = front;
+                while (temp != null)
+                {
+                    Console.Write("| " + temp.Data + " |");
+                    temp = temp.Next;
+                }
+            }
+        }
+    }
+}
